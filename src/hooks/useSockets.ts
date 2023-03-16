93ebafac02923cb4aa7154f032ee3c3e681a84d4
Jordan Maslyn <jordan.maslyn@wpengine.com>
2023-03-16 13:41:34 -0400

    Use WS transport first

--- src/hooks/useSockets.ts
+++ src/hooks/useSockets.ts
@@ -1,36 +1,40 @@
-import { useEffect, useState } from "react"
-import io, { Socket } from 'socket.io-client'
+import { useEffect, useState } from 'react';
+import io, { Socket } from 'socket.io-client';
 
 export function useSockets(roomId: string) {
-    const [socket, setSocket] = useState(null);
+  const [socket, setSocket] = useState(null);
 
-    useEffect(() => {
-        if (!roomId) return;
+  useEffect(() => {
+    if (!roomId) return;
 
+    fetch('/api/socketio').finally(() => {
+      const socket = io({ transports: ['websocket', 'polling'] });
+      setSocket(socket);
 
-        fetch('/api/socketio').finally(() => {
-            const socket = io();
-            setSocket(socket)
+      socket.on('connect', () => {
+        console.log('connect');
+        socket.emit('hello', roomId);
+      });
 
-            socket.on('connect', () => {
-                console.log('connect');
-                socket.emit('hello', roomId);
-            })
+      socket.on('a user connected', () => {
+        console.log('a user connected');
+      });
 
-            socket.on('a user connected', () => {
-                console.log('a user connected')
-            })
+      socket.on('disconnect', () => {
+        console.log('disconnect');
+      });
 
-            socket.on('disconnect', () => {
-                console.log('disconnect')
-            })
-        });
+      socket.on('connect_error', () => {
+        // revert to classic upgrade
+        socket.io.opts.transports = ['polling', 'websocket'];
+      });
+    });
 
-        return () => {
-            console.log('cleaning up useSockets hook');
-            socket?.disconnect();
-        }
-    }, [roomId])
+    return () => {
+      console.log('cleaning up useSockets hook');
+      socket?.disconnect();
+    };
+  }, [roomId]);
 
-    return socket;
+  return socket;
 }
